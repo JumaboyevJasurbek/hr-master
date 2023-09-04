ebe8e06662fcfdc4c4629a68d249e1f528c775fe
JumaboyevJasurbek <jumaboyevjasurbek2004@gmail.com>
2023-09-04 09:28:14 -0700

    update database

--- src/utils/pg.js
+++ src/utils/pg.js
@@ -1,19 +1,18 @@
-import pg from "pg"
-import dotenv from 'dotenv'
-dotenv.config()
-
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const a = async (SQL, ...params) => {
     const client = new pg.Client({
-        connectionString: 'postgres://hnhgrral:9-UQ5lEvm4vjRor9VXI1Ksytz7hBQOnH@topsy.db.elephantsql.com/hnhgrral'
-    })
-    try {      
-        await client.connect()
-        const res = await client.query(SQL, params)
-        return res?.rows || []
+        connectionString: "postgres://postgres:DJ9USbp%iLou@localhost/hr_master",
+    });
+    try {
+        await client.connect();
+        const res = await client.query(SQL, params);
+        return res?.rows || [];
     } catch (error) {
-        console.log(error)
+        console.log(error);
     } finally {
-        await client.end()
+        await client.end();
     }
-}
+};
